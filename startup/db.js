d6a2cce32888e06e98b3bde67a11590a89b3c323
Imadi <newuser@gmail.com>
2019-07-09 18:01:42 +0500

    generic message

--- startup/db.js
+++ startup/db.js
@@ -6,7 +6,8 @@ const config = require('../config/custom-environment-variables.json')
 module.exports = function(){
     mongoose.connect('mongodb+srv://clusterUser:123abc@vidly-x1n7u.mongodb.net/test?retryWrites=true' , { useNewUrlParser: true });
     mongoose.connection.once('open',()=>{
-        winston.info(config.db);
+        //winston.info('Connection made');
+        console.log(config.db);
     }).on('error', (error)=>{
         console.log('Connection error', error);
     });
