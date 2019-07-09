0a6db18e5c3319b18f6d86df60be824680aa2a4f
Imadi <newuser@gmail.com>
2019-07-09 14:53:27 +0500

    Add mongo support 8

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 
 
 module.exports = function(){
-    mongoose.connect('mongodb://127.0.0.1/Cluster0' , { useNewUrlParser: true });
+    mongoose.connect('mongodb://localhost/vidly' , { useNewUrlParser: true });
     mongoose.connection.once('open',()=>{
         winston.info('Connection made');
     }).on('error', (error)=>{
