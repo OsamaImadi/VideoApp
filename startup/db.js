87d15d4f3486f1bec8074c2f4444df898183f062
Imadi <newuser@gmail.com>
2019-07-09 17:52:22 +0500

    generic message

--- startup/db.js
+++ startup/db.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const winston = require('winston');
+const config = require('../config/custom-environment-variables.json')
 
 
 module.exports = function(){
-    mongoose.connect('mongodb://localhost/vidly' , { useNewUrlParser: true });
+    mongoose.connect('mongodb+srv://clusterUser:123abc@vidly-x1n7u.mongodb.net/test?retryWrites=true' , { useNewUrlParser: true });
     mongoose.connection.once('open',()=>{
-        winston.info('Connection made');
+        winston.info(config.db);
     }).on('error', (error)=>{
         console.log('Connection error', error);
     });
 } 
+
+//mongodb://localhost/vidly
